e3833b68ccbd83627435d33b2763608343f8e43e
Vaibhav8171 <vaibhavkrishnatra789@gmail.com>
2024-11-25 12:02:01 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,16 @@
+// let Impir = document.querySelector("#Ename")
+// let dat = document.querySelector("#match_date")
+// let linkk = document.querySelector("#submit")
+
+
+// if(Impir.value==''){
+//   alert("please fill up the all required information")
+// }
+// else{
+//   linkk.style.displ=
+// }
+
+// linkk.addEventListener('click',()=>{
+//   if()
+// })
+
